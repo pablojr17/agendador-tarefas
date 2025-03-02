209d4eaf7aacac9012ac990241459926a90ee9d2
pablojr17 <pablojr17@gmail.com>
2025-03-02 15:11:20 -0300

    Adicionadas configuracoes de seguranca

---
+++ src/main/java/com/pablo/agendadortarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.pablo.agendadortarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+}
